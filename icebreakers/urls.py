2f2810ad9394221b2d2bc963d34387d311fa16ed
Avery Jones <averyj@sfu.ca>
2017-10-13 23:15:47 -0700

    cleaning up file insertions

--- icebreakers/urls.py
+++ icebreakers/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include, url
+from django.contrib import admin
 
 urlpatterns = [
+    url(r'^homepage/', include('homepage.urls')),
     url(r'^admin/', admin.site.urls),
 ]
